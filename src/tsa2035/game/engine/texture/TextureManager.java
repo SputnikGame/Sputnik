feac6234e0ec9a97d5c7c896030f2effac74cf4d
Andrew <andrew@lobos.me>
2013-01-02 13:59:57 -0500

    Added more debug output

--- src/tsa2035/game/engine/texture/TextureManager.java
+++ src/tsa2035/game/engine/texture/TextureManager.java
@@ -1,28 +1,43 @@
 package tsa2035.game.engine.texture;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 
 public class TextureManager {
 	private static HashMap<String, StaticTexture> textures = new HashMap<String, StaticTexture>();
 	
-	public static StaticTexture getTexture(String path) throws FileNotFoundException, IOException
+	public static StaticTexture getTexture(String path) throws IOException
 	{
 		if ( textures.containsKey(path) )
 			return textures.get(path);
-		StaticTexture t = new StaticTexture(path);
-		textures.put(path, t);
-		return t;
+		try
+		{
+			StaticTexture t = new StaticTexture(path);
+			textures.put(path, t);
+			return t;
+		}
+		catch( IOException e )
+		{
+			System.out.println("Failed to load texture "+path);
+			throw e;
+		}
 	}
 
-	public static StaticTexture getTextureFromResource(String path) throws FileNotFoundException, IOException
+	public static StaticTexture getTextureFromResource(String path) throws IOException
 	{
 		if ( textures.containsKey(path) )
 			return textures.get(path);
 		System.out.println("Loading texture "+path);
-		StaticTexture t = new StaticTexture(TextureManager.class.getResourceAsStream(path));
-		textures.put(path, t);
-		return t;
+		try
+		{
+			StaticTexture t = new StaticTexture(TextureManager.class.getResourceAsStream(path));
+			textures.put(path, t);
+			return t;
+		}
+		catch ( IOException e )
+		{
+			System.out.println("Failed to load texture "+path);
+			throw e;
+		}
 	}
 }
