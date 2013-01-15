de3bc1c5e41c4a1fa85bab9ff97ac0803f7cad2c
Andrew <andrew@lobos.me>
2013-01-15 14:30:07 -0500

    Added parser for bitmap font info files

---
+++ src/tsa2035/game/engine/fontparser/BitmapFontData.java
@@ -0,0 +1,39 @@
+package tsa2035.game.engine.fontparser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class BitmapFontData {
+	private ArrayList<String> pages = new ArrayList<String>();
+	private HashMap<Character, HashMap<String, Integer>> glyphData = new HashMap<Character, HashMap<String, Integer>>();
+	
+	public BitmapFontData(String filePath) throws IOException
+	{
+		BufferedReader reader = new BufferedReader(new InputStreamReader(BitmapFontData.class.getResourceAsStream(filePath)));
+		while ( reader.ready() )
+		{
+			String line = reader.readLine();
+			String words[] = line.replaceAll("\\s+", " ").split(" ");
+			
+			if ( words[0].equals("char") )
+			{
+				HashMap<String, Integer> glyph = new HashMap<String, Integer>();
+				for ( int i = 1; i < words.length; i++ )
+				{
+					String thisValue[] = words[i].split("=");
+					glyph.put(thisValue[0], Integer.parseInt(thisValue[1]));
+				}
+				glyphData.put((char)Character.toChars(glyph.get("id"))[0], glyph);
+			}
+			
+		}
+	}
+	
+	public HashMap<String, Integer> getDataForGlyph(char character)
+	{
+		return glyphData.get(character);
+	}
+}
