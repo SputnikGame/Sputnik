7ec97770e4969e9ec297aaac62cfd84ff64ffe33
Andrew <andrew@lobos.me>
2013-01-24 10:52:40 -0500

    Added Level1 and game main

---
+++ src/tsa2035/game/content/core/Game.java
@@ -0,0 +1,19 @@
+package tsa2035.game.content.core;
+
+import org.lwjgl.LWJGLException;
+
+import tsa2035.game.content.levels.Level1;
+import tsa2035.game.engine.core.Renderer;
+
+public class Game {
+
+	/**
+	 * @param args
+	 * @throws LWJGLException 
+	 */
+	public static void main(String[] args) throws LWJGLException {
+		Renderer.init(800, 600);
+		Renderer.renderLoop(new Level1());
+	}
+
+}
