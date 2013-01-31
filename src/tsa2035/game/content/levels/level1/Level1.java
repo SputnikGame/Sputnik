fb38e8ae11be68de2bead5255a44c670b57b0995
Nick Joniec <njoniec@gmail.com>
2013-01-31 18:16:49 -0500

    Added oxygen canister

--- src/tsa2035/game/content/levels/level1/Level1.java
+++ src/tsa2035/game/content/levels/level1/Level1.java
@@ -16,6 +16,7 @@ public class Level1 extends Scene {
 			setBackground(new SpriteBackground(TextureManager.getTextureFromResource("/tsa2035/game/content/images/common/wallpanels.png")));
 			addToScene("character", new MainCharacter(0,0));
 			addToScene("floor", new Sprite(0f, -0.98f, TextureManager.getTextureFromResource("/tsa2035/game/content/images/common/floor.png"))).setSolid(true);
+			addToScene("canister", new Sprite(0f, -0.5f, TextureManager.getTextureFromResource("/tsa2035/game/content/images/common/canister.png"))).setInteractable(true);
 		} catch (IOException e) {
 			System.out.println("Texture loading failed!");
 			e.printStackTrace();
