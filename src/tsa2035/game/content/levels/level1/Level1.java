e35c047e99bf6a7936aa36f2b7db21650efcde6a
Nick Joniec <njoniec@gmail.com>
2013-02-03 22:04:17 -0500

    Added doors, platforms, ladders and pipes

--- src/tsa2035/game/content/levels/level1/Level1.java
+++ src/tsa2035/game/content/levels/level1/Level1.java
@@ -16,7 +16,9 @@ public class Level1 extends Scene {
 			setBackground(new SpriteBackground(TextureManager.getTextureFromResource("/tsa2035/game/content/images/common/wallpanels.png")));
 			addToScene("character", new MainCharacter(-0.8f, -0.5f));
 			addToScene("floor", new Sprite(0f, -0.98f, TextureManager.getTextureFromResource("/tsa2035/game/content/images/common/floor.png"))).setSolid(true);
-			//addToScene("canister", new Sprite(0f, -0.5f, TextureManager.getTextureFromResource("/tsa2035/game/content/images/common/canister.png"))).setInteractable(true);
+			addToScene("canister", new Sprite(0f, -0.4f, TextureManager.getTextureFromResource("/tsa2035/game/content/images/common/ladder_long.png"))).setInteractable(true);
+			addToScene("pipes", new Sprite(0f, 0.7f, TextureManager.getTextureFromResource("/tsa2035/game/content/images/common/pipes.png"))).setSolid(true);
+			addToScene("platform", new Sprite(0f, 0.5f, TextureManager.getTextureFromResource("/tsa2035/game/content/images/common/door.png")));
 		} catch (IOException e) {
 			System.out.println("Texture loading failed!");
 			e.printStackTrace();
