74e063bc3fc8670e5419c4b296cd94b15f8b99c7
Andrew <andrew@lobos.me>
2013-02-05 08:00:24 -0500

    Ladders with platforms

--- src/tsa2035/game/engine/scene/Ladder.java
+++ src/tsa2035/game/engine/scene/Ladder.java
@@ -4,10 +4,18 @@ import tsa2035.game.engine.texture.Texture;
 
 public class Ladder extends Sprite {
 	String playerName;
+	String platformName = null;
 	boolean wasUsing = false;
+	
 	public Ladder(float x, float y, Texture t, String playerName) {
 		super(x, y, t);
 		this.playerName = playerName;
+	}
+	
+	public Ladder(float x, float y, Texture t, String playerName, String platformName) {
+		super(x, y, t);
+		this.playerName = playerName;
+		this.platformName = platformName;
 	}
 	
 	public void render(Scene parent)
@@ -15,15 +23,24 @@ public class Ladder extends Sprite {
 		super.render(parent);
 
 		Player player = (Player)parent.getObject(playerName);
+		Sprite platform = null;
+		
+		if ( platformName != null )
+		 platform = parent.getObject(platformName);
+		
 		boolean contacting = player.contacting(this);
 		if ( wasUsing && !contacting )
 		{
 			player.setGravity(true);
+			if ( platformName != null )
+				platform.setSolid(true);
 			player.setJumpingDisabled(false);
 		}
 		else if ( contacting )
 		{
 			player.setGravity(false);
+			if ( platformName != null )
+				platform.setSolid(false);
 			player.setJumpingDisabled(true);
 		}
 		wasUsing = contacting;
